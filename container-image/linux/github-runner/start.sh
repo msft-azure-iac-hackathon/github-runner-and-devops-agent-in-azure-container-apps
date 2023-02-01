9d6e95c1ed52b875677d94111c2f522641f42026
Bogdan Grozoiu <grozoiu@hotmail.com>
2023-02-01 12:18:58 +0100

    ghr running with sudo and some optimizations

--- container-image/linux/github-runner/start.sh
+++ container-image/linux/github-runner/start.sh
@@ -3,18 +3,19 @@
 GH_OWNER=$GH_OWNER
 GH_REPOSITORY=$GH_REPOSITORY
 GH_TOKEN=$GH_TOKEN
-
 RUNNER_NAME=$(hostname)
 
 REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
-cd /home/docker/actions-runner
-chmod +x ./run.sh
 
-mkdir _work
-mkdir _work/$(hostname)
+echo $REG_TOKEN
+ls -asl
+mkdir -p ./_work/$(hostname)
+ls -asl
 
-echo "Configuring runner..."
+export RUNNER_ALLOW_RUNASROOT="1"
+
+echo "Configuring runner ${RUNNER_NAME}...}"
 echo https://github.com/${GH_OWNER}/${GH_REPOSITORY}
 ./config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} \
     --token ${REG_TOKEN} \
@@ -22,13 +23,20 @@ echo https://github.com/${GH_OWNER}/${GH_REPOSITORY}
     --labels pwsh,azps,azcli,"$(lsb_release -si):$(lsb_release -sr)" \
     --work _work/$(hostname)
 
+
 cleanup() {
-    echo "Removing runner..."
-    ./config.sh remove --token ${REG_TOKEN}
+    echo "Removing runner ${RUNNER_NAME}...}"
+    REM_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/remove-token | jq .token --raw-output)
+    echo ${REM_TOKEN}
+    ./config.sh remove --token ${REM_TOKEN}
     echo "Removing work directory..."
-    rm -rf ${_work/$(hostname)}
+    ls -asl _work
+    rm -r _work/$(hostname)
+    ls -asl _work
+    echo "Bye!"
 }
 
+trap 'cleanup; exit 0' EXIT
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
